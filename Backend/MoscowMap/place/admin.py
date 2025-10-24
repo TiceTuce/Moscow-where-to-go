1ae2ea7a987ea154c2c9f1d53cc751c815635b7a
Rolba <matygulin03@mail.ru>
2025-10-24 14:26:02 +0700

    simplify photo adding

--- Backend/MoscowMap/place/admin.py
+++ Backend/MoscowMap/place/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import Place, Photo 
 
+
+class PhotoInline(admin.TabularInline):
+    verbose_name = 'Фотография'
+    verbose_name_plural = 'Фотографии'
+    model = Photo
+
+
 # Register your models here.
-admin.site.register(Place)
-admin.site.register(Photo)
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lng', 'lat')
+    inlines = (PhotoInline,)
--- Backend/MoscowMap/place/models.py
+++ Backend/MoscowMap/place/models.py
@@ -1,14 +1,37 @@
 from django.db import models
 
 class Place(models.Model):
-    title = models.CharField(max_length=120)
-    description_short = models.TextField(max_length=500)
-    description_long = models.TextField(max_length=1500)
-    lng = models.FloatField()
-    lat = models.FloatField()
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+    
+    title = models.CharField(
+        max_length=120, 
+        verbose_name='Название'
+    )
+    description_short = models.TextField(
+        max_length=500,
+        verbose_name='Короткое описание'    
+    )
+    description_long = models.TextField(
+        max_length=1500,
+        verbose_name='Полное описание'
+    )
+    lng = models.FloatField(verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта')
+    
+    def __str__(self):
+        return self.title
     
 
 class Photo(models.Model):
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="places")
+    class Meta:
+        ordering = ["position"]
+    
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Место')
+    image = models.ImageField(upload_to="places", verbose_name='Картинка')
+    position = models.IntegerField(default=1, verbose_name='Позиция')
+    
+    def __str__(self):
+        return self.image.url
     
