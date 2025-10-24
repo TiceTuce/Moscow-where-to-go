5e3db7f6ce5b3377da01b34d5d657088fd22d549
Rolba <matygulin03@mail.ru>
2025-10-24 09:06:46 +0700

    create data model

---
+++ Backend/MoscowMap/place/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Place(models.Model):
+    title = models.CharField(max_length=120)
+    description_short = models.TextField(max_length=500)
+    description_long = models.TextField(max_length=1500)
+    lng = models.FloatField()
+    lat = models.FloatField()
+    
+
+class Photo(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images")
+    
